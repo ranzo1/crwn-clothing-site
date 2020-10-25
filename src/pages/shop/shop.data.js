9f2bcca4c261f2f6dc2e7290a2353671eb6e5da5
protic <protic80@gmail.com>
2020-10-25 21:12:25 +0100

    Add header component

--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -37,36 +37,36 @@ const SHOP_DATA = [
         {
           id: 5,
           name: 'Green Beanie',
-          // imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
-          imageUrl: "/images/shop-img/hats/green-beanie.png",
+           imageUrl: 'https://i.ibb.co/YTjW3vF/green-beanie.png',
+          //imageUrl: "/images/shop-img/hats/green-beanie.png",
           price: 18
         },
         {
           id: 6,
           name: 'Palm Tree Cap',
-          // imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
-          imageUrl: "/images/shop-img/hats/palm-tree-cap.png",
+           imageUrl: 'https://i.ibb.co/rKBDvJX/palm-tree-cap.png',
+          //imageUrl: "/images/shop-img/hats/palm-tree-cap.png",
           price: 14
         },
         {
           id: 7,
           name: 'Red Beanie',
-          // imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
-          imageUrl: "/images/shop-img/hats/red-beanie.png",
+           imageUrl: 'https://i.ibb.co/bLB646Z/red-beanie.png',
+          //imageUrl: "/images/shop-img/hats/red-beanie.png",
           price: 18
         },
         {
           id: 8,
           name: 'Wolf Cap',
-          // imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
-          imageUrl: "/images/shop-img/hats/wolf-cap.png",
+           imageUrl: 'https://i.ibb.co/1f2nWMM/wolf-cap.png',
+          //imageUrl: "/images/shop-img/hats/wolf-cap.png",
           price: 14
         },
         {
           id: 9,
           name: 'Blue Snapback',
-          // imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
-          imageUrl: "/images/shop-img/hats/blue-snapback.png",
+           imageUrl: 'https://i.ibb.co/X2VJP2W/blue-snapback.png',
+          //imageUrl: "/images/shop-img/hats/blue-snapback.png",
           price: 16
         }
       ]
@@ -79,8 +79,8 @@ const SHOP_DATA = [
         {
           id: 10,
           name: 'Adidas NMD',
-          // imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
-          imageUrl: "/images/shop-img/sneakers/adidas-nmd.png", 
+           imageUrl: 'https://i.ibb.co/0s3pdnc/adidas-nmd.png',
+          //imageUrl: "/images/shop-img/sneakers/adidas-nmd.png", 
           price: 220
         },
         {
