7c625dad1d7e62e2536b9f14b6dc63bee40254e0
protic <protic80@gmail.com>
2020-11-12 14:33:04 +0100

    Integration of stripe, updating favicon and title of a site

---
+++ src/components/stripe-button/stripe-button.component.js
@@ -0,0 +1,30 @@
+import React from "react";
+import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    "pk_test_51Hm5AFCdLwlgc0XP0Y8Ic8iD5yBn0CgF093eQegMXOmoTkIBzVcRjI4zaoKcBYwvwfVUPdlq4YK0PXFuPwOm6Jl400AoWpxzGh";
+
+  const onToken = (token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay now"
+      name="King-Shop"
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
