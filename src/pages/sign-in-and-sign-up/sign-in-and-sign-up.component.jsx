b91d0424f87a16b931bb32f45ec9e42eef97ff95
protic <protic80@gmail.com>
2020-11-24 16:45:29 +0100

    Implemented mobile support beta

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,14 +1,15 @@
-import React from "react";
+import React from 'react';
 
-import "./sign-in-and-sign-up.styles.scss";
-import SignIn from "../../components/sign-in/sign-in.component";
-import SignUp from "../../components/sign-up/sign-up.component";
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
-const SignInAndSignOut = () => (
-  <div className="sign-in-and-sign-up">
+import { SignInAndSignUpContainer } from './sign-in-and-sign-up.styles';
+
+const SignInAndSignUpPage = () => (
+  <SignInAndSignUpContainer>
     <SignIn />
-    <SignUp/>
-  </div>
+    <SignUp />
+  </SignInAndSignUpContainer>
 );
 
-export default SignInAndSignOut;
+export default SignInAndSignUpPage;
